c0e5882c878158f31ed47e16df0820d558aaa033
Warren Stix <me@warrenstix.xyz>
2022-12-21 15:03:30 -0700

    making dwm more in line with gruvbox

--- config.h
+++ config.h
@@ -8,8 +8,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=32" };
 static const char dmenufont[]       = "monospace:size=32";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#ebdbb2";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
